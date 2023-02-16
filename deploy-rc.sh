d974d2db12100cc6a26272b696cbfbf59a3e9f06
Cyberphoenix90 <Joao260690@gmail.com>
2023-02-16 11:06:17 +0100

    add install script, create artifact from deploy script

--- deploy-rc.sh
+++ deploy-rc.sh
@@ -1,11 +1,17 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 SOURCE_FOLDER="${SCRIPT_DIR}/projects/photonlang/Photon/out/bin/RC/net7.0/";
+PARENT_FOLDER="${SCRIPT_DIR}/projects/photonlang/Photon/out/bin/";
 TARGET_FOLDER="${SCRIPT_DIR}/projects/photonlang/Photon/out/bin/Release/";
 IN_FILE="${SCRIPT_DIR}/projects/photonlang/Photon/out/bin/Release/net7.0/Photon";
 
 mkdir -p ${TARGET_FOLDER};
 cp -ar ${SOURCE_FOLDER} ${TARGET_FOLDER};
+
+# Create an artifact for the release by ziping the release folder
+cd ${PARENT_FOLDER};
+zip -r photon.zip Release;
+mv photon.zip ${SCRIPT_DIR};
 
 OUT_FILE="/usr/bin/ph";
 
---
+++ install.sh
@@ -0,0 +1,16 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TARGET_FOLDER="${SCRIPT_DIR}/projects/photonlang/Photon/out/bin/";
+
+mkdir -p ${TARGET_FOLDER};
+
+# Extract the release artifact
+unzip photon.zip -d ${TARGET_FOLDER};
+
+OUT_FILE="/usr/bin/ph";
+
+if [ -f "$OUT_FILE" ]; then
+    echo "Photon CLI Already Installed";
+else
+    echo "Installing";
+    sudo ln -s ${IN_FILE} ${OUT_FILE};
+fi
